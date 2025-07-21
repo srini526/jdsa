a44042422d29cf07974a6ed046d94f39fc80e5a7
srini526 <kangaresrinivas@gmail.com>
2025-07-21 11:56:22 +0530

    uifhygfr

---
+++ Prog1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Prog1{
+    public static void main(String[] args) {
+        recursion(5);
+        
+    }
+    public static void recursion(int n){
+        if(n==0){
+            return;
+        }
+        recursion(n-1);
+        System.out.println(n);
+    }
+
+
+        
+}
