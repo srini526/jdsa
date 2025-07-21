4a4ecdc8c4f67eb3679e06e4d6c7bfc6de2c45f8
srini526 <kangaresrinivas@gmail.com>
2025-07-21 12:04:48 +0530

    rgyuudg

--- Prog1.java
+++ Prog1.java
@@ -4,14 +4,19 @@ class Prog1{
         recursion(5);
         
     }
+    // public static void recursion(int n){
+    //     if(n==0){
+    //         return;
+    //     }
+    //     recursion(n-1);
+    //     System.out.println(n);
+    // }
     public static void recursion(int n){
         if(n==0){
             return;
         }
+        System.out.print(n+" ");
         recursion(n-1);
-        System.out.println(n);
     }
 
-
-        
 }
