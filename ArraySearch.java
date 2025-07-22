c56ce1c31593d76afcd88af03a1a8de838c92401
srini526 <kangaresrinivas@gmail.com>
2025-07-22 15:18:12 +0530

    rf

---
+++ ArraySearch.java
@@ -0,0 +1,12 @@
+public class ArraySearch {
+    public static void main(String[] args){
+        int [] a ={1,2,3,4,5,6,7,8,9};
+        int key = 5;
+        for(int i=0;i<a.length;i++){
+             if (a[i]==key){
+            System.out.println("The Element is at index: "+i);
+        }
+        }
+       
+    }
+}
