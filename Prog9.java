b3f8ff7cbd05071338581c4c2c5f2706987a5c27
srini526 <kangaresrinivas@gmail.com>
2025-07-22 12:21:26 +0530

    sihsh

--- Prog9.java
+++ Prog9.java
@@ -1,6 +1,9 @@
+import java.util.*;
 public class Prog9 {
     public static void main(String[] args) {
-        int n=5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 if(i%2 !=0){
