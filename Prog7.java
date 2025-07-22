08290621b6637663443b0a52cfaec829af34811b
srini526 <kangaresrinivas@gmail.com>
2025-07-22 12:00:01 +0530

    sihsh

--- Prog7.java
+++ Prog7.java
@@ -1,3 +1,15 @@
 public class Prog7 {
-    
+    public static void main(String[] args) {
+        int n=4;
+        for(int i = 0;i < n; i++){
+             for(int j = 0;j < n; j++){
+                if( i == 0 || i == (n-1) || j == 0 || j == (n-1)){
+                     System.out.print("*");
+                } else{
+                     System.out.print(" ");
+                }           
+            }
+            System.out.println();
+        }     
+    }
 }
