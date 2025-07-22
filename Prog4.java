31a197f4aa2218db3db27342ea80193a92acc8b7
srini526 <kangaresrinivas@gmail.com>
2025-07-22 07:40:37 +0530

    sfkejj

---
+++ Prog4.java
@@ -0,0 +1,11 @@
+public class Prog4 {
+    public static void main(String[] args){
+        int n = 5;
+        int result = fibonacciRecursion(n);
+        System.out.println("The " + n + "th Fibonacci number is: " + result);
+    }
+    static int fibonacciRecursion(int n) {
+        if (n <= 1) return n;
+        return fibonacciRecursion(n - 1) + fibonacciRecursion(n - 2);
+    }
+}
